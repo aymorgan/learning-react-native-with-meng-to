b99155d0493b1456bbab25bc72d8e68031fd399d
Adam Morgan <aymorgan@hotmail.co.uk>
2019-12-27 20:12:00 +0000

    Finish the Redux class - toggleMenu and show animation

--- App.js
+++ App.js
@@ -1,210 +1,29 @@
 import React from 'react';
-import { Text, SafeAreaView, ScrollView } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 
-import styled from 'styled-components';
-import Card from './components/Card';
-import Course from './components/Course';
-import { Ionicons } from '@expo/vector-icons';
-import Logo from './components/Logo';
-import Menu from './components/Menu';
+const initialState = {
+  action: ""
+};
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Menu />
-        <SafeAreaView>
-          <ScrollView>
+const reducer = (state = initialState, action) => {
 
-            <TitleBar>
-            <Avatar source={require('./assets/avatar.jpg')}></Avatar>
-              <Title>Welcome back,</Title>
-              <Name>Adam Morgan</Name>
-              <Ionicons
-                name="ios-notifications"
-                size={28}
-                color="#4775f2"
-                style={{ position: "absolute", right: 20, top: 15}}
-              />
-            </TitleBar>
-
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ padding: 20, paddingLeft: 16, paddingTop:30, flexDirection: 'row'}}>
-
-              {logos.map((logo, index) => (
-                <Logo key={index} image={logo.image} text={logo.text} />
-              ))}
-
-            </ScrollView>
-
-            <SubTitle>
-              Continue Learning
-            </SubTitle>
-
-            <ScrollView horizontal={true} style={{ paddingBottom: 30}} showsHorizontalScrollIndicator={false}>
-              {cards.map((card, index) => (
-                <Card
-                  key={index}
-                  title={card.title}
-                  image={card.image}
-                  caption={card.caption}
-                  logo={card.logo}
-                  subtitle={card.subtitle}
-                  avatar={card.avatar}
-                />
-              ))}
-            </ScrollView>
-
-            <SubTitle>
-              Popular Courses
-            </SubTitle>
-
-            <ScrollView >
-              {courses.map((course, index) => (
-                <Course
-                  key={index}
-                  image={course.image}
-                  title={course.title}
-                  subtitle={course.subtitle}
-                  logo={course.logo}
-                  caption={course.caption}
-                  avatar={course.avatar}
-                  subtitle={course.subtitle}
-                  author={course.author}
-                />
-              ))}
-            </ScrollView>
-
-
-          </ScrollView>
-        </SafeAreaView>
-      </Container>
-    );
+  switch(action.type) {
+    case "OPEN_MENU": 
+      return { action: "openMenu"};
+    case "CLOSE_MENU":
+      return { action: "closeMenu"};
+    default: return state;
   }
-}
+};
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #f0f3f5;
-`;
+const store = createStore(reducer);
 
-const Title = styled.Text`
-  font-size: 16px;
-  color:#b8bece;
-  font-weight: 500;
-`;
+const App = () => (
+  <Provider store={store}>
+    <HomeScreen />
+  </Provider>
+)
 
-const Name = styled.Text`
-  font-size: 20px;
-  color:#3c4560;
-  font-weight: bold;
-`;
-
-const TitleBar = styled.View`
-  width: 100%;
-  margin-top: 20px;
-  padding-left:80px;
-`;
-
-const Avatar = styled.Image`
-  width:44px;
-  height:44px;
-  border-radius:22px;
-  margin-left:20px;
-  position: absolute;
-  left:0;
-  top:0;
-`;
-
-const SubTitle = styled.Text`
-  color:#b8bece;
-  font-weight:600;
-  font-size: 15px;
-  margin-left:20px;
-  margin-top:20px;
-  text-transform:uppercase;
-`;
-
-
-
-const logos = [
-  {
-    image: require('./assets/logo-framerx.png'),
-    text: "Framer X",
-  },
-  {
-    image: require('./assets/logo-sketch.png'),
-    text: "Sketch",
-  },
-  {
-    image: require('./assets/logo-invision.png'),
-    text: "Invision",
-  },
-  {
-    image: require('./assets/logo-figma.png'),
-    text: "Figma",
-  },
-  {
-    image: require('./assets/logo-studio.png'),
-    text: "Studio X",
-  },
-]
-
-const cards = [
-  {
-    title: "Senior Product Designer at Careem",
-    image: require('./assets/background8.jpg'),
-    subtitle: "React Native",
-    caption: "1 of 12 sections",
-    logo: require('./assets/logo-react.png'),
-
-  },
-  {
-    title: "Senior Product Designer at Dubizzle",
-    image: require('./assets/background6.jpg'),
-    subtitle: "React Native",
-    caption: "1 of 12 sections",
-    logo: require('./assets/logo-react.png'),
-
-  },
-  {
-    title: "Senior Product Designer at Bank of Dubai",
-    image: require('./assets/background7.jpg'),
-    subtitle: "React Native",
-    caption: "1 of 12 sections",
-    logo: require('./assets/logo-react.png'),
-
-  },
-]
-
-const courses = [
-  {
-    title: "Prototype in Invision Studio",
-    subtitle: "10 Sections",
-    image: require('./assets/background13.jpg'),
-    logo: require('./assets/logo-studio.png'),
-    author: "Adam Morgan",
-    avatar: require('./assets/avatar.jpg'),
-    caption: "Hello and welcome to the show",
-
-  },
-  {
-    title: "Prototype in Invision Studio",
-    subtitle: "10 Sections",
-    image: require('./assets/background1.jpg'),
-    logo: require('./assets/logo-studio.png'),
-    author: "Adam Morgan",
-    avatar: require('./assets/avatar.jpg'),
-    caption: "Hello and welcome to the show - this is three lines sdasd",
-
-  },
-  {
-    title: "Prototype in Framer X",
-    subtitle: "10 Sections",
-    image: require('./assets/background9.jpg'),
-    logo: require('./assets/logo-framerx.png'),
-    author: "Adam Morgan",
-    avatar: require('./assets/avatar.jpg'),
-    caption: "Hello and welcome to the show",
-
-  },
-]
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,282 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, TouchableOpacity, Animated, Easing, StatusBar } from 'react-native';
+
+import styled from 'styled-components';
+import Card from '../components/Card';
+import Course from '../components/Course';
+import { Ionicons } from '@expo/vector-icons';
+import Logo from '../components/Logo';
+import Menu from '../components/Menu';
+import { connect } from "react-redux";
+
+function mapStateToProps(state) {
+  return { action: state.action }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    openMenu: () => dispatch({
+      type: "OPEN_MENU"
+    })
+  }
+}  
+
+class HomeScreen extends React.Component {
+
+  state = {
+    scale: new Animated.Value(1),
+    opacity: new Animated.Value(1)
+  };
+
+  componentDidMount() {
+    StatusBar.setBarStyle("dark-content", true);
+  }
+
+  componentDidUpdate() {
+    this.toggleMenu()
+  };
+
+  toggleMenu = () => {
+    if (this.props.action == "openMenu") {
+      Animated.timing(this.state.scale, {
+        toValue: 0.9,
+        duration: 300,
+        easing: Easing.in()
+      }).start();
+      Animated.spring(this.state.opacity, {
+        toValue: 0.5
+      }).start();
+
+      StatusBar.setBarStyle("light-content", true);
+    }
+
+    if (this.props.action == "closeMenu") {
+      Animated.timing(this.state.scale, {
+        toValue: 1,
+        duration: 300,
+        easing: Easing.in()
+      }).start();
+      Animated.spring(this.state.opacity, {
+        toValue: 1
+      }).start();
+
+      StatusBar.setBarStyle("dark-content", true);
+    }
+  };
+
+
+
+  render() {
+    return (
+      <RootView>
+        <Menu />
+      <AnimatedContainer style={{ transform: [{ scale: this.state.scale,}], opacity: this.state.opacity  }}>
+        
+        <SafeAreaView>
+          <ScrollView>
+
+            <TitleBar>
+            <TouchableOpacity onPress={this.props.openMenu} style={{ position: "absolute", top: 0, left: 0 }}>
+              <Avatar source={require('../assets/avatar.jpg')}></Avatar>
+            </TouchableOpacity>
+              <Title>Welcome back,</Title>
+              <Name>Adam Morgan</Name>
+              <Ionicons
+                name="ios-notifications"
+                size={28}
+                color="#4775f2"
+                style={{ position: "absolute", right: 20, top: 15}}
+              />
+            </TitleBar>
+
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ padding: 20, paddingLeft: 16, paddingTop:30, flexDirection: 'row'}}>
+
+              {logos.map((logo, index) => (
+                <Logo key={index} image={logo.image} text={logo.text} />
+              ))}
+
+            </ScrollView>
+
+            <SubTitle>
+              Continue Learning
+            </SubTitle>
+
+            <ScrollView horizontal={true} style={{ paddingBottom: 30}} showsHorizontalScrollIndicator={false}>
+              {cards.map((card, index) => (
+                <Card
+                  key={index}
+                  title={card.title}
+                  image={card.image}
+                  caption={card.caption}
+                  logo={card.logo}
+                  subtitle={card.subtitle}
+                  avatar={card.avatar}
+                />
+              ))}
+            </ScrollView>
+
+            <SubTitle>
+              Popular Courses
+            </SubTitle>
+
+            <ScrollView >
+              {courses.map((course, index) => (
+                <Course
+                  key={index}
+                  image={course.image}
+                  title={course.title}
+                  subtitle={course.subtitle}
+                  logo={course.logo}
+                  caption={course.caption}
+                  avatar={course.avatar}
+                  subtitle={course.subtitle}
+                  author={course.author}
+                />
+              ))}
+            </ScrollView>
+
+
+          </ScrollView>
+        </SafeAreaView>
+      </AnimatedContainer>
+      </RootView>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+
+const RootView = styled.View`
+  background: black;
+  flex: 1;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+  border-radius: 10px;
+  overflow:hidden;
+`;
+
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
+
+const Title = styled.Text`
+  font-size: 16px;
+  color:#b8bece;
+  font-weight: 500;
+  margin-left:20px;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color:#3c4560;
+  font-weight: bold;
+  margin-left:20px;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 20px;
+  padding-left:60px;
+`;
+
+const Avatar = styled.Image`
+  width:44px;
+  height:44px;
+  border-radius:22px;
+  margin-left:20px;
+`;
+
+const SubTitle = styled.Text`
+  color:#b8bece;
+  font-weight:600;
+  font-size: 15px;
+  margin-left:20px;
+  margin-top:20px;
+  text-transform:uppercase;
+`;
+
+
+
+const logos = [
+  {
+    image: require('../assets/logo-framerx.png'),
+    text: "Framer X",
+  },
+  {
+    image: require('../assets/logo-sketch.png'),
+    text: "Sketch",
+  },
+  {
+    image: require('../assets/logo-invision.png'),
+    text: "Invision",
+  },
+  {
+    image: require('../assets/logo-figma.png'),
+    text: "Figma",
+  },
+  {
+    image: require('../assets/logo-studio.png'),
+    text: "Studio X",
+  },
+]
+
+const cards = [
+  {
+    title: "Senior Product Designer at Careem",
+    image: require('../assets/background8.jpg'),
+    subtitle: "React Native",
+    caption: "1 of 12 sections",
+    logo: require('../assets/logo-react.png'),
+
+  },
+  {
+    title: "Senior Product Designer at Dubizzle",
+    image: require('../assets/background6.jpg'),
+    subtitle: "React Native",
+    caption: "1 of 12 sections",
+    logo: require('../assets/logo-react.png'),
+
+  },
+  {
+    title: "Senior Product Designer at Bank of Dubai",
+    image: require('../assets/background7.jpg'),
+    subtitle: "React Native",
+    caption: "1 of 12 sections",
+    logo: require('../assets/logo-react.png'),
+
+  },
+]
+
+const courses = [
+  {
+    title: "Prototype in Invision Studio",
+    subtitle: "10 Sections",
+    image: require('../assets/background13.jpg'),
+    logo: require('../assets/logo-studio.png'),
+    author: "Adam Morgan",
+    avatar: require('../assets/avatar.jpg'),
+    caption: "Hello and welcome to the show",
+
+  },
+  {
+    title: "Prototype in Invision Studio",
+    subtitle: "10 Sections",
+    image: require('../assets/background1.jpg'),
+    logo: require('../assets/logo-studio.png'),
+    author: "Adam Morgan",
+    avatar: require('../assets/avatar.jpg'),
+    caption: "Hello and welcome to the show - this is three lines sdasd",
+
+  },
+  {
+    title: "Prototype in Framer X",
+    subtitle: "10 Sections",
+    image: require('../assets/background9.jpg'),
+    logo: require('../assets/logo-framerx.png'),
+    author: "Adam Morgan",
+    avatar: require('../assets/avatar.jpg'),
+    caption: "Hello and welcome to the show",
+
+  },
+]
